3a95c194cb9dd24c574619c5afcf22b74ea4287c
zereshk001 <ninjapeak1384@gmail.com>
2025-01-26 13:23:44 +0330

    i chenged the time for send backup in to channel into 10 min

--- json to txt cookies.py
+++ json to txt cookies.py
@@ -25,14 +25,32 @@ from http.cookies import SimpleCookie
 # # استفاده از این تابع
 # json_to_netscape("cookies.json", "cookies.txt")
 
-from telegram import Bot
+import requests
+from bs4 import BeautifulSoup
 
-TOKEN = "7667164344:AAGexmRDdWX-uWoolMtVB46lEMkqO2SKkPE"
-bot = Bot(token=TOKEN)
+def download_pinterest_image(url):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+    }
 
-async def get_chat_id():
-    chat = await bot.get_chat("@backup_mega_helper")
-    print(chat.id)
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.text, "html.parser")
 
-import asyncio
-asyncio.run(get_chat_id())
+    # پیدا کردن لینک تصویر
+    image_tag = soup.find("meta", property="og:image")
+    if image_tag:
+        image_url = image_tag["content"]
+        print(f"✅ لینک عکس پیدا شد: {image_url}")
+
+        # دانلود و ذخیره تصویر
+        img_data = requests.get(image_url).content
+        with open("pinterest_image.jpg", "wb") as img_file:
+            img_file.write(img_data)
+        print("✅ عکس با موفقیت دانلود شد.")
+    else:
+        print("❌ عکس پیدا نشد!")
+
+# تست با لینک پینترست
+pinterest_url = "https://www.pinterest.com/pin/7036943162210025/"  # لینک نمونه
+download_pinterest_image(pinterest_url)
+
